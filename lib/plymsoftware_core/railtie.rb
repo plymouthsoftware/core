e351e2bed602b59a80988e17e6d1137e24e0401b
Chris Blunt <chris@plymouthsoftware.com>
2012-02-17 14:04:07 +0000

    Namespace in Railtie.

--- lib/plymsoftware_core/railtie.rb
+++ lib/plymsoftware_core/railtie.rb
@@ -3,7 +3,7 @@ require "plymsoftware_core/action_view/helpers/content_helpers"
 module PlymsoftwareCore
   class Railtie < Rails::Railtie
     initializer "plymsoftware_core.view_helpers" do |app|
-      ActionView::Base.send :include, ActionView::Helpers::ContentHelpers
+      ::ActionView::Base.send :include, ActionView::Helpers::ContentHelpers
     end
   end
 end
