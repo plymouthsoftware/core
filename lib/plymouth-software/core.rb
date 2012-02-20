bcf3b717e658c1e7e73837ceafbbe9cc22bd7f41
Chris Blunt <chris@plymouthsoftware.com>
2012-02-20 13:36:17 +0000

    Tweak Railtie.

--- lib/plymouth-software/core.rb
+++ lib/plymouth-software/core.rb
@@ -3,4 +3,6 @@ require "plymouth-software/core/version"
 require "plymouth-software/core/active_record"
 require "plymouth-software/core/action_controller"
 
+require "plymouth-software/core/action_view/helpers/content_helpers"
+
 require "plymouth-software/core/railtie" if defined? Rails
--- lib/plymouth-software/core/railtie.rb
+++ lib/plymouth-software/core/railtie.rb
@@ -1,13 +1,8 @@
-require "plymouth-software/core/action_view/helpers/content_helpers"
-
 module PlymouthSoftware
   module Core
     class Railtie < Rails::Railtie
-      initializer "plymouth-software.core" do |app|
+      initializer "plymouth-software.core.action-view.helpers" do |app|
         ::ActionView::Base.send :include, ActionView::Helpers::ContentHelpers
-        ::ActionController::Base.extend ActionController::Authentication
-        ::ActiveRecord::Base.extend ActiveRecord::Priceable
-        ::ActiveRecord::Base.extend ActiveRecord::User
       end
     end
   end
