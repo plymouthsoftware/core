70053e5246fc422ee19000a45438710cca130d11
Chris Blunt <chris@plymouthsoftware.com>
2013-02-26 15:08:07 +0000

    Fix railtie.

--- lib/plymouth-software/core/railtie.rb
+++ lib/plymouth-software/core/railtie.rb
@@ -1,7 +1,7 @@
 module PlymouthSoftware
   module Core
     class Railtie < ::Rails::Railtie
-      ::ActionView::Base.extend, ActionView::Helpers::ContentHelpers
+      ::ActionView::Base.send :include, ActionView::Helpers::ContentHelpers
       ::ActiveRecord::Base.extend ActiveRecord::User
       ::ActiveRecord::Base.extend ActiveRecord::Priceable
       ::ActionController::Base.extend ActionController::Authentication
